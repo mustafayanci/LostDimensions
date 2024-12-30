3e8271ac668334dc1adf278d9f8506e3b9908e82
Mustafa Yanci <mustafayanci@gmail.com>
2024-12-31 02:44:33 +0300

    auto: 2024-12-31_02-44-33 - updates detected and deployed

--- Assets/Scripts/Puzzle/PuzzleBase.cs
+++ Assets/Scripts/Puzzle/PuzzleBase.cs
@@ -1,1 +1,44 @@
- 
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class PuzzleBase : MonoBehaviour, IDimensionAware
+{
+    [Header("Puzzle Base Settings")]
+    [SerializeField] protected int[] activeDimensions;
+    [SerializeField] protected bool isSolved;
+    
+    public UnityEvent onPuzzleSolved;
+    public UnityEvent onPuzzleReset;
+    
+    protected virtual void Start()
+    {
+        ResetPuzzle();
+    }
+
+    public virtual void OnDimensionChanged(int dimensionId)
+    {
+        bool isActiveInDimension = System.Array.Exists(activeDimensions, d => d == dimensionId);
+        gameObject.SetActive(isActiveInDimension);
+        
+        if (!isActiveInDimension)
+        {
+            ResetPuzzle();
+        }
+    }
+
+    protected virtual void SolvePuzzle()
+    {
+        if (isSolved) return;
+        
+        isSolved = true;
+        onPuzzleSolved?.Invoke();
+    }
+
+    protected virtual void ResetPuzzle()
+    {
+        if (!isSolved) return;
+        
+        isSolved = false;
+        onPuzzleReset?.Invoke();
+    }
+} 
