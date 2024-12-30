ae4e30d4b0e37a2a0d5576d51ac5d46af419123e
Mustafa Yanci <mustafayanci@gmail.com>
2024-12-31 02:41:48 +0300

    auto: 2024-12-31_02-41-48 - updates detected and deployed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,1 +1,54 @@
- 
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float horizontalInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Yatay hareket inputunu al
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        // Zıplama kontrolü
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+
+        // Zeminde olup olmadığını kontrol et
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+        // Karakteri hareket ettir
+        Move();
+    }
+
+    private void Move()
+    {
+        // Yatay hareketi uygula
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+} 
