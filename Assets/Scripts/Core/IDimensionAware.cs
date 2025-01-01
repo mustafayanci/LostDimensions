c92752b29c017805950a1cdbda2e9dbf6ff40903
Mustafa Yanci <mustafayanci@gmail.com>
2025-01-01 04:52:09 +0300

    auto: 2025-01-01_04-52-09 - updates detected and deployed

--- Assets/Scripts/Core/IDimensionAware.cs
+++ Assets/Scripts/Core/IDimensionAware.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 
+/// <summary>
+/// Boyut değişiminden etkilenen nesneler için interface
+/// </summary>
 public interface IDimensionAware
 {
+    /// <summary>
+    /// Boyut değiştiğinde çağrılır
+    /// </summary>
+    /// <param name="dimensionId">Yeni boyut ID'si</param>
     void OnDimensionChanged(int dimensionId);
 } 
