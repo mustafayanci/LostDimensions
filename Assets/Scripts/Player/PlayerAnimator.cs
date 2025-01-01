e0ace0ffb9e9499ac4ba6c21850ec1f428e76919
Mustafa Yanci <mustafayanci@gmail.com>
2025-01-01 04:25:22 +0300

    auto: 2025-01-01_04-25-22 - updates detected and deployed

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,1 +1,48 @@
- 
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(SpriteRenderer))]
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private PlayerController controller;
+    private Rigidbody2D rb;
+
+    private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private static readonly int VerticalVelocity = Animator.StringToHash("verticalVelocity");
+    private static readonly int IsDashing = Animator.StringToHash("isDashing");
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        controller = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Hareket yönüne göre sprite'ı çevir
+        if (rb.velocity.x != 0)
+        {
+            spriteRenderer.flipX = rb.velocity.x < 0;
+        }
+
+        // Animator parametrelerini güncelle
+        animator.SetBool(IsGrounded, Physics2D.Raycast(transform.position, Vector2.down, 0.1f));
+        animator.SetBool(IsRunning, Mathf.Abs(rb.velocity.x) > 0.1f);
+        animator.SetFloat(VerticalVelocity, rb.velocity.y);
+        animator.SetBool(IsDashing, false); // PlayerController'dan dash durumunu al
+    }
+
+    public void PlayHurtAnimation()
+    {
+        animator.SetTrigger("Hurt");
+    }
+
+    public void PlayDeathAnimation()
+    {
+        animator.SetTrigger("Death");
+    }
+} 
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement Settings")]
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 10f;
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpForce = 16f;
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashDuration = 0.2f;
     
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private bool isGrounded;
-    private float horizontalInput;
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashTimeLeft;
+    private Vector2 moveInput;
+    private bool jumpInput;
+    private bool dashInput;
 
     private void Awake()
     {
@@ -22,33 +30,82 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Yatay hareket inputunu al
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+        // Input
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump")) jumpInput = true;
+        if (Input.GetButtonDown("Dash")) dashInput = true;
 
-        // Zıplama kontrolü
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            Jump();
-        }
-
-        // Zeminde olup olmadığını kontrol et
+        // Ground check
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     private void FixedUpdate()
     {
-        // Karakteri hareket ettir
-        Move();
+        if (isDashing)
+        {
+            HandleDash();
+            return;
+        }
+
+        // Movement
+        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+
+        // Jump
+        if (jumpInput && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            AudioManager.Instance.PlaySound("PlayerJump");
+        }
+
+        // Dash
+        if (dashInput && canDash)
+        {
+            StartDash();
+        }
+
+        // Reset inputs
+        jumpInput = false;
+        dashInput = false;
     }
 
-    private void Move()
+    private void StartDash()
     {
-        // Yatay hareketi uygula
-        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+        isDashing = true;
+        canDash = false;
+        dashTimeLeft = dashDuration;
+        rb.gravityScale = 0;
+        AudioManager.Instance.PlaySound("PlayerDash");
     }
 
-    private void Jump()
+    private void HandleDash()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        if (dashTimeLeft > 0)
+        {
+            rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0);
+            dashTimeLeft -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            isDashing = false;
+            rb.gravityScale = 3;
+            if (isGrounded) canDash = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canDash = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = isGrounded ? Color.green : Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 } 
