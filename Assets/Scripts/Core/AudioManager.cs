b273bc7276c5301ae505bf6a345ca9b6169935e0
Mustafa Yanci <mustafayanci@gmail.com>
2025-01-01 04:18:10 +0300

    auto: 2025-01-01_04-18-10 - updates detected and deployed

--- Assets/Scripts/Core/AudioManager.cs
+++ Assets/Scripts/Core/AudioManager.cs
@@ -1,1 +1,88 @@
- 
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        public AudioMixerGroup mixerGroup;
+        [Range(0f, 1f)] public float volume = 1f;
+        [Range(0.1f, 3f)] public float pitch = 1f;
+        public bool loop;
+        
+        [HideInInspector] public AudioSource source;
+    }
+
+    [Header("Audio Settings")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Sound[] sounds;
+    
+    private Dictionary<string, Sound> soundDictionary = new Dictionary<string, Sound>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            InitializeAudio();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void InitializeAudio()
+    {
+        foreach (Sound s in sounds)
+        {
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+            s.source = source;
+
+            source.clip = s.clip;
+            source.outputAudioMixerGroup = s.mixerGroup;
+            source.volume = s.volume;
+            source.pitch = s.pitch;
+            source.loop = s.loop;
+
+            soundDictionary[s.name] = s;
+        }
+    }
+
+    public void PlaySound(string name)
+    {
+        if (soundDictionary.TryGetValue(name, out Sound sound))
+        {
+            sound.source.Play();
+        }
+        else
+        {
+            Debug.LogWarning($"Sound: {name} not found!");
+        }
+    }
+
+    public void StopSound(string name)
+    {
+        if (soundDictionary.TryGetValue(name, out Sound sound))
+        {
+            sound.source.Stop();
+        }
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+    }
+} 
