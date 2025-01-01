ff535714a9a7e66b7fa32f5d0660293bafe97594
Mustafa Yanci <mustafayanci@gmail.com>
2025-01-01 04:10:31 +0300

    auto: 2025-01-01_04-10-31 - updates detected and deployed

--- Assets/Scripts/Editor/GameInitializer.cs
+++ Assets/Scripts/Editor/GameInitializer.cs
@@ -1,1 +1,26 @@
- 
+using UnityEngine;
+using UnityEditor;
+
+public class GameInitializer : Editor
+{
+    [MenuItem("Game/Initialize Game")]
+    public static void InitializeGame()
+    {
+        // 1. Proje yapısını oluştur
+        ProjectSetup.SetupProjectStructure();
+        
+        // 2. UI sistemini kur
+        UISetup.SetupUI();
+        
+        // 3. Transition sistemini kur
+        TransitionSetup.SetupTransitionUI();
+        
+        // 4. GameCore'u oluştur
+        CoreSetup.SetupGameCore();
+        
+        // 5. Sahneleri oluştur
+        SceneSetup.SetupInitialScenes();
+        
+        Debug.Log("Game initialization completed!");
+    }
+} 
